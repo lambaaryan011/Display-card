43bc19ea87fda7bc9ec70b7070158aeb0f758123
lambaaryan011 <aryanlamba54@gmail.com>
2024-03-01 23:07:22 +0530

    Display card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './component/./introCrd.css' 
+import Tasksss from "./component/Tasksss"
+
+function App() {
+  return (
+    <>
+    <Tasksss display={false} />
+   
+    </>
+  );
+}
+
+export default App;
